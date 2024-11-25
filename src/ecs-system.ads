7eb264bbdc38e79567c303cf6046691fc23f23e1
Page, Christopher <chrispage7@gmail.com>
2024-11-24 22:20:01 -0500

    Collision system added (prelim)

--- src/ecs-system.ads
+++ src/ecs-system.ads
@@ -1,28 +1,27 @@
-with ecs.component; use ecs.component;
-with ecs.entity; use ecs.entity;
+with ECS.Component; use ECS.Component;
+with ECS.Entity; use ECS.Entity;
 with Ada.Tags; use Ada.Tags;
+with ECS.Entity_Manager; use ECS.Entity_Manager;
+package ECS.System is
 
-package ecs.system is
+   type System_T is interface;
 
-    type System_T is interface;
-
-    procedure Execute (Self : System_T;
+   procedure Execute ( Self : System_T;
                        Dt   : Duration; 
-                       E    : access Entity_T'Class;  
-                       ES   : Entities_T := Entities_T'(1 .. 0 => null)) is abstract;
+                       Manager    : access Entity_Manager_T'Class ) is abstract;
 
-    type System_Access is access all System_T'Class;
+   type System_Access is access all System_T'Class;
 
-    type Mover_T is new System_T with null record;
-    procedure Execute (Self : Mover_T;
+   type Mover_T is new System_T with null record;
+
+   procedure Execute ( Self : Mover_T;
                        Dt   : Duration; 
-                       E    : access Entity_T'Class;  
-                       ES   : Entities_T := Entities_T'(1 .. 0 => null));
+                       Manager    : access Entity_Manager_T'Class );
 
-    type Collision_T is new System_T with null record;
-    procedure Execute (Self : Collision_T;
+   type Collision_T is new System_T with null record;
+
+   procedure Execute ( Self : Collision_T;
                        Dt   : Duration;
-                       E    : access Entity_T'Class;
-                       ES   : Entities_T := Entities_T'(1 .. 0 => null));
+                       Manager    : access Entity_Manager_T'Class );
 
-end ecs.system;
+end ECS.System;
