7658deb77f70d74bb4d8ec10c4eaa7995ea071b2
Page, Christopher <chrispage7@gmail.com>
2024-12-05 11:53:29 -0500

    Moved the game loop from the project main to a test file for now.

--- src/psu_ada_gameengine.adb
+++ src/psu_ada_gameengine.adb
@@ -7,42 +7,7 @@ with System;
 with Renderer; use Renderer;
 with Ada.Real_Time; use Ada.Real_Time;
 with ECS.Color; use ECS.Color;
-procedure Psu_Ada_Gameengine is
-Title : Unbounded_String := To_Unbounded_String("Game Window");
-GameWindow : Window_Access;
-Buffer : Win32.Byte_Array(0 .. 800 * 600 * 4) := (others => 0);
-
-SkyBlue : Color := (R => 135, G => 206, B => 236, A => 255);
-
-Start_Time, Stop_Time : Time;
-Elapsed_Time          : Time_Span;
-Counter : Float := 1.0;              
+procedure Psu_Ada_Gameengine is            
 begin
-   Start_Time := Clock;
-
-   GameWindow := New_Window(800,600,Title);
    Put_Line ("Start Engine");
-
-   declare
-      Message        : MSG_Access := new MSG;
-      Has_Msg        : Boolean := True;
-      Lp_Result      : LRESULT;
-   begin
-      while Has_Msg loop
-      Stop_Time := Clock;
-      Elapsed_Time := Stop_Time - Start_Time;
-      Lp_Result := Dispatch_Message (Message);
-      Has_Msg := Get_Message (Message, System.Null_Address, 0, 0);
-
-               --  -- Process emitted events here
-               --  Process_Events(Manager.all);
-      if To_Duration(Elapsed_Time) > 1.0/60.0 then
-         Clear_Screen(Buffer, SkyBlue, 800, 600);
-         Draw_Regular_Polygon(Buffer, 20, 50, 50.0 + Counter, 50.0, ECS.Color.Red, 800);
-         GameWindow.Draw_Buffer(Buffer'Address);
-         Start_Time := Clock;
-         Counter := Counter + 50.0;
-      end if;
-      end loop;
-   end;
 end Psu_Ada_Gameengine;
